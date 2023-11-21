5b3699440249c06941ed2c9dbbadf2b296e4f1b9
anne247 <149846863+anne247@users.noreply.github.com>
2023-11-20 20:00:27 -0600

    vpc creation

---
+++ main.tf
@@ -0,0 +1,40 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a new Custom VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+# Create 1st public subnet
+resource "aws_subnet" "eks_pub_sub_one" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Pub Subnet One"
+  }
+}
+
+
+# Create 2nd public subnet
+resource "aws_subnet" "eks_pub_sub_two" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Pub Subnet two"
+  }
+}
